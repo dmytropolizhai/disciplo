287b35e7ac4c995d1ef2bcd6adb943aba3f21ffd
Dmytro <dmytropolizhai@gmail.com>
2026-01-02 14:29:11 +0200

    feat(language-type): add type guard
    Signed-off-by: Dmytro <dmytropolizhai@gmail.com>

--- src/types/language.ts
+++ src/types/language.ts
@@ -1,3 +1,7 @@
 import { resources } from '@/i18n';
 
 export type Language = keyof typeof resources;
+
+export function isLanguage<T>(value: T | Language ): value is Language {
+  return (value as Language) in resources;
+}
