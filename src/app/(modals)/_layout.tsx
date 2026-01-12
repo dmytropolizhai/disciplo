3ae4397a9c3aba31c978fd053c0afed43dbfe25a
Dmytro Polizhai <dmytropolizhai@gmail.com>
2026-01-12 08:30:04 +0200

    feat(create-task): add modal with logic to create task

--- src/app/(modals)/_layout.tsx
+++ src/app/(modals)/_layout.tsx
@@ -1,4 +1,12 @@
+import { useTranslation } from "@/hooks/use-translation";
+import { Stack } from "expo-router";
 
-export function ModalsLayout() {
-    return null;
+export default function ModalsLayout() {
+    const { t } = useTranslation("modals")
+    return (
+        <Stack screenOptions={{ presentation: "modal" }}>
+            <Stack.Screen name="create-task" options={{ title: t("create-task.title") }}/>
+
+        </Stack>
+    );
 }
--- src/app/(modals)/create-task.tsx
+++ src/app/(modals)/create-task.tsx
@@ -1,12 +1,16 @@
-import { TodoCard } from "@/components/feature/todo/todo-card";
-import { Input, type InputProps } from "@/components/ui/input";
-import { Label, type LabelProps } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { useTranslation } from "@/hooks/use-translation";
-import { View } from "react-native";
-import DateTimePicker from "@react-native-community/datetimepicker"
-import { PropsWithChildren, useState } from "react";
-import { Button } from "@/components/ui/button";
+import { Task, View } from "react-native";
+import { ChangeEvent, PropsWithChildren, useState } from "react";
+import { Button, ButtonProps } from "@/components/ui/button";
 import { Target } from "lucide-react-native";
+import { Text } from "@/components/ui/text";
+import DateTimePicker from "@react-native-community/datetimepicker"
+import { AddButton } from "@/components/shared/add-button";
+import { useTaskStore } from "@/stores/use-task-store";
+import { NewTask } from "@/database/types";
+import { router } from "expo-router";
 
 const today = new Date();
 
@@ -24,24 +28,27 @@ const Field = ({ name, label, children }: FieldProps) => (
 )
 
 
-const DeadlinePicker = () => {
+const DeadlinePicker = (props: Omit<ButtonProps, "onPress">) => {
     const [show, setShow] = useState<boolean>(false);
     const [date, setDate] = useState<Date>(today);
 
     return (
         <>
-            <Button variant="outline" onPressIn={() => setShow(true)}>
+            <Button {...props} onPress={() => setShow(true)}>
+                <Text>Select</Text>
                 <Target />
             </Button>
             {show && (
                 <DateTimePicker 
                     testID="deadlinePicker"
-                    value={date}
+                    value={today}
                     mode="datetime"
                     is24Hour
                     onChange={(_, selectedDate) => {
                         setShow(false)
-                        if (selectedDate) setDate(selectedDate)
+                        if (selectedDate) {
+                            setDate(selectedDate)
+                        }
                     }}
 
                 />
@@ -51,22 +58,40 @@ const DeadlinePicker = () => {
 
 }
 
-
 export default function CreateTaskModal() {
     const { t } = useTranslation("modals.create-task")
+    const [newTask, setNewTask] = useState<NewTask | undefined>(undefined)
+    const createTask = useTaskStore(state => state.createTask)
+
+    function handleCreateTask() {
+        if (!newTask) {
+            throw new Error("Can't create empty task!")
+        }
+        createTask(newTask)
+        router.navigate("/todo")
+    }
+
+    function handleChange<K extends keyof NewTask>(key: K, value: NewTask[K]) {
+        setNewTask(prev => ({
+            ...(prev ?? {} as NewTask),
+            [key]: value
+        }))
+    }
 
     return (
-       <View className="w-full h-full flex items-center gap-4 p-4">
-            <Field name="task-name" label={t("input.task-name.name")}>
-                <Input placeholder={t("input.task-name.placeholder")} />
-            </Field>
-            <Field name="task-description" label={t("input.task-description.name")}>
-                <Input placeholder={t("input.task-description.placeholder")} />
-            </Field>
-                    
-            <Field name="task-deadline" label={t("input.task-deadline")} >
-                <DeadlinePicker />
-            </Field>
+        <View className="w-full h-full justify-between pr-4 pl-4">
+           <View className="w-full flex items-center gap-4">
+                <Field name="task-name" label={t("input.task-name.name")}>
+                    <Input placeholder={t("input.task-name.placeholder")} onChangeText={text => handleChange("name", text)} />
+                </Field>
+                <Field name="task-description" label={t("input.task-description.name")}>
+                    <Input placeholder={t("input.task-description.placeholder")} onChangeText={text => handleChange("description", text)}/>
+                </Field>
+                <Field name="task-deadline" label={t("input.task-deadline.name")} >
+                    <DeadlinePicker variant="outline" />
+                </Field>
+            </View>
+            <AddButton className="mb-8" onPress={handleCreateTask}/>
         </View>
     )
 }
